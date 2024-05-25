ce2fa9f887a5ea7d411985ae3e5455ce3d195345
Luis Parrado <luisprmat@gmail.com>
2024-05-25 13:04:23 -0500

    Endpoint to get categories. resource + route + controller (backend)

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,3 +10,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::get('posts', [PostController::class, 'index']);
+Route::get('categories', [CategoryController::class, 'index']);
