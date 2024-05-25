419c254f2b9b9f28756a28727a62667275174992
Luis Parrado <luisprmat@gmail.com>
2024-05-25 15:46:55 -0500

    Filter by category: backend

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -5,12 +5,19 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class PostController extends Controller
 {
     public function index(): AnonymousResourceCollection
     {
-        return PostResource::collection(Post::with('category')->paginate(10));
+        $posts = Post::with('category')
+            ->when(request('category'), function (Builder $query) {
+                $query->where('category_id', request('category'));
+            })
+            ->paginate(10);
+
+        return PostResource::collection($posts);
     }
 }
