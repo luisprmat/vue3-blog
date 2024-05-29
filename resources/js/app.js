b9417d6590a554f9e46e52de799ee9ef3f043d6f
Luis Parrado <luisprmat@gmail.com>
2024-05-28 23:36:05 -0500

    Routes in a Separate File

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,21 +2,10 @@ import './bootstrap'
 import '../css/app.css'
 
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
 import { i18nVue } from 'laravel-vue-i18n'
 import App from '@/App.vue'
-import PostCreate from '@/Components/Posts/Create.vue'
-import PostIndex from '@/Components/Posts/Index.vue'
 
-const routes = [
-  { path: '/', name: 'posts.index', component: PostIndex },
-  { path: '/posts/create', name: 'posts.create', component: PostCreate },
-]
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-})
+import router from '@/routes'
 
 createApp(App)
   .use(router)
