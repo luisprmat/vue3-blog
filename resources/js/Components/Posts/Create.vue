19c1c6b456e73107a1f10e2805dd5c05bb804480
Luis Parrado <luisprmat@gmail.com>
2024-05-30 00:00:30 -0500

    Vue Submit: Component Method

--- resources/js/Components/Posts/Create.vue
+++ resources/js/Components/Posts/Create.vue
@@ -1,6 +1,7 @@
 <script setup>
-import { onMounted } from 'vue'
+import { onMounted, reactive } from 'vue'
 import useCategories from '@/composables/categories'
+import usePosts from '@/composables/posts'
 
 import InputLabel from '@/Components/InputLabel.vue'
 import TextInput from '@/Components/TextInput.vue'
@@ -9,35 +10,51 @@ import SelectInput from '@/Components/SelectInput.vue'
 import PrimaryButton from '@/Components/PrimaryButton.vue'
 
 const { categories, getCategories } = useCategories()
+const { storePost } = usePosts()
 
-const submit = () => {
-  console.log('submitted')
-}
+const post = reactive({
+  title: '',
+  content: '',
+  category_id: '',
+})
 
 onMounted(getCategories)
 </script>
 
 <template>
-  <form @submit.prevent="submit">
+  <form @submit.prevent="storePost(post)">
     <!-- Title -->
     <div>
       <InputLabel for="post-title" :value="$t('Title')" />
 
-      <TextInput id="post-title" class="mt-1 block w-full" autofocus />
+      <TextInput
+        v-model="post.title"
+        id="post-title"
+        class="mt-1 block w-full"
+        autofocus
+      />
     </div>
 
     <!-- Content -->
     <div class="mt-4">
       <InputLabel for="post-content" :value="$t('Content')" />
 
-      <TextareaInput id="post-content" class="mt-1 block w-full" />
+      <TextareaInput
+        v-model="post.content"
+        id="post-content"
+        class="mt-1 block w-full"
+      />
     </div>
 
     <!-- Category -->
     <div class="mt-4">
       <InputLabel for="post-category" :value="$t('Category')" />
 
-      <SelectInput id="post-category" class="mt-1 block w-full">
+      <SelectInput
+        v-model="post.category_id"
+        id="post-category"
+        class="mt-1 block w-full"
+      >
         <option value="" selected>-- {{ $t('Choose category') }} --</option>
         <option v-for="category in categories" :value="category.id">
           {{ category.name }}
