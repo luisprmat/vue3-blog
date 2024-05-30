085ec1c54bb03ca80733dd87b02209fea84ba7b0
Luis Parrado <luisprmat@gmail.com>
2024-05-29 22:57:40 -0500

    HTML form in vue, custom primary button

--- resources/js/Components/Posts/Create.vue
+++ resources/js/Components/Posts/Create.vue
@@ -1,1 +1,39 @@
-<template>Crear post</template>
+<script setup>
+import InputLabel from '@/Components/InputLabel.vue'
+import TextInput from '@/Components/TextInput.vue'
+import TextareaInput from '@/Components/TextareaInput.vue'
+import SelectInput from '@/Components/SelectInput.vue'
+import PrimaryButton from '@/Components/PrimaryButton.vue'
+</script>
+
+<template>
+  <form>
+    <!-- Title -->
+    <div>
+      <InputLabel for="post-title" :value="$t('Title')" />
+
+      <TextInput id="post-title" class="mt-1 block w-full" autofocus />
+    </div>
+
+    <!-- Content -->
+    <div class="mt-4">
+      <InputLabel for="post-content" :value="$t('Content')" />
+
+      <TextareaInput id="post-content" class="mt-1 block w-full" />
+    </div>
+
+    <!-- Category -->
+    <div class="mt-4">
+      <InputLabel for="post-category" :value="$t('Category')" />
+
+      <SelectInput id="post-category" class="mt-1 block w-full">
+        <option value="" selected>-- {{ $t('Choose category') }} --</option>
+      </SelectInput>
+    </div>
+
+    <!-- Buttons -->
+    <div class="mt-4">
+      <PrimaryButton>{{ $t('Save') }}</PrimaryButton>
+    </div>
+  </form>
+</template>
